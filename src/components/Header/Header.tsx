869a47cc1549d6ac00bab13b2d054f7643674819
Sergey Maksimov <s.mksimov@gmail.com>
2022-11-29 00:08:10 +0300

    fixup Background

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,24 +3,26 @@ import style from './Header.module.scss'
 export const Header = () => {
   return (
     <header className={style.header}>
-      <div className={style.logo}>
-        <a href="/" className={style.logoImg}>
-          <img src="/logo_velbex_1.svg" alt="logo" />
-          <span className={style.rightLogo}>
+      <div className={style.flex}>
+        <div className={style.logo}>
+          <a href="/" className={style.logoImg}>
+            <img src="/logo_velbex_1.svg" alt="logo" />
+            <span className={style.rightLogo}>
           <img src="/logo_velbex_2.svg" alt="velbex" />
         </span>
-        </a>
-        <div className={style.logoText}>крупный интегратор CRM в&nbsp;Росcии и&nbsp;ещё&nbsp;8&nbsp;странах</div>
+          </a>
+          <div className={style.logoText}>крупный интегратор CRM в&nbsp;Росcии и&nbsp;ещё&nbsp;8&nbsp;странах</div>
+        </div>
+        <menu>
+          <ul className={style.menu}>
+            <li className={style.item}><a href="#">Услуги</a></li>
+            <li className={style.item}><a href="#">Виджеты</a></li>
+            <li className={style.item}><a href="#">Интеграция</a></li>
+            <li className={style.item}><a href="#">Кейсы</a></li>
+            <li className={style.item}><a href="#">Сертификаты</a></li>
+          </ul>
+        </menu>
       </div>
-      <menu>
-        <ul className={style.menu}>
-          <a href="#"><li className={style.item}>Услуги</li></a>
-          <a href="#"><li className={style.item}>Виджеты</li></a>
-          <a href="#"><li className={style.item}>Интеграция</li></a>
-          <a href="#"><li className={style.item}>Кейсы</li></a>
-          <a href="#"><li className={style.item}>Сертификаты</li></a>
-        </ul>
-      </menu>
       <div className={style.contacts}>
         <a href="tel:+75555555555">+7&nbsp;555&nbsp;555-55-55</a>
         <a href="#"><img className={style.item} src="/telegram.svg" alt="telegram" /></a>
--- src/components/Layout/Background/Background.tsx
+++ src/components/Layout/Background/Background.tsx
@@ -8,7 +8,9 @@ export const Background: FC<BackgroundProps> = ({ children }) => {
   return (
     <div className={styles.backdrop}>
       {children}
-      {/*<div className={styles.test}></div>*/}
+      <div className={styles.purpleBall}></div>
+      <div className={styles.redBall}></div>
+      <div className={styles.smallRedBall}></div>
       <div className={styles.redLight}></div>
       <div className={styles.purpleLight}></div>
     </div>
